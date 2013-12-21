bd70073a5f2574838c61f13c50620546b643e18b
Geoff Crittenden <geoffcritt@hotmail.com>
2013-12-20 21:42:12 -0600

    added #is_land? and #both_land?

--- world.rb
+++ world.rb
@@ -8,4 +8,17 @@ class World
     { lat: 0 - coords[:lat], lon: 180 + coords[:lon] }
   end
 
+  def is_land?(coords)
+    # API Magic goes here
+
+  end
+
+  def both_land?(coords)
+    if is_land?(coords) && is_land?(opposite(coords))
+      true
+    else
+      false
+    end    
+  end
+
 end
