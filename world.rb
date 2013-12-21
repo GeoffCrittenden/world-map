f44d40111d584b56533abbab2420afccbc689bab
Geoff Crittenden <geoffcritt@hotmail.com>
2013-12-20 19:51:32 -0600

    created method for opposite coords

--- world.rb
+++ world.rb
@@ -1,2 +1,18 @@
 class World
+
+  def initialize(map_data)
+    @map_data = map_data
+  end
+
+  def opposite(coords)
+    opp_coords = {}
+    opp_coords[:lat] = 0 - coords[:lat]
+    if coords[:lon] > 0
+      opp_coords[:lon] = coords[:lon] - 180
+    else
+      opp_coords[:lon] = 180 + coords[:lon]
+    end
+    opp_coords
+  end
+
 end
