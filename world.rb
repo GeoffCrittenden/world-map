7966e926a0fe4c2260d4d32319ec52c938d75b48
Geoff Crittenden <geoffcritt@hotmail.com>
2013-12-20 22:32:58 -0600

    updated World to Location and changed related variables

--- world.rb
+++ world.rb
@@ -1,20 +1,23 @@
-class World
+class Location
+  attr_reader :coords, :opp_coords
 
-  def initialize
+  def initialize(coords)
+    @coords     = coords
+    @opp_coords = opposite
   end
 
-  def opposite(coords)
-    return { lat: 0 - coords[:lat], lon: coords[:lon] - 180 } if coords[:lon] > 0
-    { lat: 0 - coords[:lat], lon: 180 + coords[:lon] }
+  def opposite
+    return { lat: 0 - @coords[:lat], lon: @coords[:lon] - 180 } if @coords[:lon] > 0
+    { lat: 0 - @coords[:lat], lon: 180 + @coords[:lon] }
   end
 
-  def is_land?(coords)
+  def is_land?
     # API Magic goes here
 
   end
 
-  def both_land?(coords)
-    return true if is_land?(coords) && is_land?(opposite(coords))
+  def both_land?
+    return true if is_land?(@coords) && is_land?(@opp_coords)
     false   
   end
 
