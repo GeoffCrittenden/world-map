1befb292edee3db965f48f6fc91c6c7f9ac93c1e
Geoff Crittenden <geoffcritt@hotmail.com>
2013-12-21 20:52:26 -0600

    created World#create_map method

--- world.rb
+++ world.rb
@@ -1,3 +1,22 @@
 class World
-  
+
+  def initialize
+    @map  = []
+    @lat  = 90
+    @lon  = -180
+    @step = 0.016667
+  end
+
+  def create_map
+    10800.times do {
+      row = []
+      21600.times do { |n|
+        @lon += (n * @step)
+        row << Location.new({lat:@lat,lon:@lon})
+      }
+      @map << row
+    }
+    puts @map
+  end
+
 end
